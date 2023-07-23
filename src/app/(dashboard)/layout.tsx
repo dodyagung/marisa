c7f8e8a6f06617d62da7c154e6b7c87ee67b8966
Dody <dodyags@gmail.com>
2023-07-23 11:17:01 +0700

    apple favicon

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -42,7 +42,7 @@ export default function DashboardLayout({
               <Link href="/" className="flex ml-2 md:mr-24">
                 <Image src={Logo} className="h-8 mr-3" alt="FlowBite Logo" />
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  MARISA
+                  Asset Management
                 </span>
               </Link>
             </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MARISA - Manajemen dan Rekomendasi Aset",
+  title: "Asset Management",
 };
 
 export default function RootLayout({
