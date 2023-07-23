9c77d8d23f1f833080ed10561c6e12c5f9de6e74
Dody <dodyags@gmail.com>
2023-07-23 12:57:38 +0700

    fix login wording

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -88,7 +88,7 @@ export default function DashboardLayout({
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"
                       >
-                        Keluar
+                        Logout
                       </Link>
                     </li>
                   </ul>
