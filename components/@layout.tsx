e78c400e83fc273b1f2fe84b164e821317d72779
Dody <dodyags@gmail.com>
2023-08-06 07:58:40 +0700

    set env and loading indicator

--- components/@layout.tsx
+++ components/@layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+      </Head>
       <main className={`${inter.className} bg-gray-200 dark:bg-gray-900`}>
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           {children}
