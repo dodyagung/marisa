47f570117ef2ba554310c287e4ae285106fbf104
Dody <dodyags@gmail.com>
2023-08-09 21:48:17 +0700

    metadata table

--- pages/aset/metadata/index.tsx
+++ pages/aset/metadata/index.tsx
@@ -2,51 +2,40 @@ import { useState, type ReactElement } from "react";
 import { AuthLayout, DashboardLayout } from "@/components/@layout";
 import Image from "next/image";
 import Logo from "/public/logo.png";
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { NextPageWithLayout } from "../../_app";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import nookies from "nookies";
 
-const Page: NextPageWithLayout = () => {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+type Aset = {
+  nama: string;
+  jenis: string;
+  pemilik: string;
+  lokasi: string;
+};
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+export const getServerSideProps: GetServerSideProps<{
+  data: Aset;
+}> = async (ctx) => {
+  const cookies = nookies.get(ctx);
 
-    setIsLoading(true);
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/aset", {
+    headers: {
+      Authorization: "Bearer " + cookies.access_token,
+    },
+  });
+  const data = await res.json();
 
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_API_URL + "/auth/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: event.target.username.value,
-          password: event.target.password.value,
-        }),
-      }
-    );
+  return { props: { data } };
+};
 
-    if (response.ok) {
-      const data = await response.json();
-
-      setCookie(null, "access_token", data.access_token);
-
-      alert("Login successful");
-      router.push("/");
-    } else {
-      alert("Invalid credentials");
-      setIsLoading(false);
-    }
-  };
-
+const Page: NextPageWithLayout = ({ data }: any) => {
   return (
     <>
       <Head>
-        <title>{`Login - ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
+        <title>{`Metadata - Data Aset - ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
       </Head>
       <div className="p-6 mt-16 sm:ml-64">
         <nav className="flex" aria-label="Breadcrumb">
@@ -54,7 +43,7 @@ const Page: NextPageWithLayout = () => {
             <li className="inline-flex items-center">
               <a
                 href="#"
-                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-red-600 dark:text-gray-400 dark:hover:text-white"
               >
                 <svg
                   className="w-3 h-3 mr-2.5"
@@ -65,7 +54,7 @@ const Page: NextPageWithLayout = () => {
                 >
                   <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                 </svg>
-                Home
+                Dashboard
               </a>
             </li>
             <li>
@@ -87,9 +76,9 @@ const Page: NextPageWithLayout = () => {
                 </svg>
                 <a
                   href="#"
-                  className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-red-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
                 >
-                  Projects
+                  Data Aset
                 </a>
               </div>
             </li>
@@ -111,12 +100,169 @@ const Page: NextPageWithLayout = () => {
                   />
                 </svg>
                 <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                  Flowbite
+                  Metadata
                 </span>
               </div>
             </li>
           </ol>
         </nav>
+
+        <div className="mx-auto max-w-screen-xl mt-4">
+          {/* Start coding here */}
+          <div className="bg-white dark:bg-gray-900 relative shadow-md sm:rounded-lg overflow-hidden">
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+              <div className="w-full md:w-1/2">
+                <form className="flex items-center">
+                  <label htmlFor="simple-search" className="sr-only">
+                    Cari
+                  </label>
+                  <div className="relative w-full">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      id="simple-search"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Pencarian"
+                      required
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                <div className="flex items-center space-x-3 w-full md:w-auto">
+                  <button
+                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-white   bg-red-600 rounded-lg border hover:bg-red-700"
+                    type="button"
+                  >
+                    <svg
+                      className="h-3.5 w-3.5 mr-2"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                      />
+                    </svg>
+                    Input Data
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="overflow-x-auto">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-4 py-3">
+                      Nama
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Jenis
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Pemilik
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Lokasi
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      <span className="sr-only">Actions</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((aset: any, index: number) => (
+                    <tr
+                      key={index}
+                      className="odd:bg-white even:bg-gray-50 border-b dark:border-gray-700"
+                    >
+                      <th
+                        scope="row"
+                        className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {aset.name}
+                      </th>
+                      <td className="px-4 py-3">{aset.kategori.name}</td>
+                      <td className="px-4 py-3">{aset.perusahaan.name}</td>
+                      <td className="px-4 py-3">
+                        {aset.aset_detail.detail_alamat}
+                      </td>
+                      <td className="px-4 py-3 flex items-center justify-end">
+                        <button
+                          id="apple-imac-27-dropdown-button"
+                          data-dropdown-toggle="apple-imac-27-dropdown"
+                          className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                          type="button"
+                        >
+                          <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                          </svg>
+                        </button>
+                        <div
+                          id="apple-imac-27-dropdown"
+                          className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                        >
+                          <ul
+                            className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="apple-imac-27-dropdown-button"
+                          >
+                            <li>
+                              <a
+                                href="#"
+                                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              >
+                                Show
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="#"
+                                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              >
+                                Edit
+                              </a>
+                            </li>
+                          </ul>
+                          <div className="py-1">
+                            <a
+                              href="#"
+                              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                            >
+                              Delete
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
