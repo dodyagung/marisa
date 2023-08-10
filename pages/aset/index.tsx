11fba8c2944f33027c3ca158aef41fb704a6fb46
Dody <dodyags@gmail.com>
2023-08-10 20:01:41 +0700

    tambah input aset page

--- pages/aset/index.tsx
+++ pages/aset/index.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import { GetServerSideProps } from "next";
 import nookies from "nookies";
 import { NextPageWithLayout } from "../_app";
+import Link from "next/link";
 
 type Aset = {
   nama: string;
@@ -32,7 +33,7 @@ const Page: NextPageWithLayout = ({ data }: any) => {
   return (
     <>
       <Head>
-        <title>{`Metadata - Data Aset - ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
+        <title>{`Data Aset - ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
       </Head>
       <div className="p-6 mt-16 sm:ml-64">
         <nav className="flex" aria-label="Breadcrumb">
@@ -54,31 +55,6 @@ const Page: NextPageWithLayout = ({ data }: any) => {
                 Dashboard
               </a>
             </li>
-            <li>
-              <div className="flex items-center">
-                <svg
-                  className="w-3 h-3 text-gray-400 mx-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-                <a
-                  href="#"
-                  className="ml-1 text-sm font-medium text-gray-700 hover:text-red-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
-                >
-                  Data Aset
-                </a>
-              </div>
-            </li>
             <li aria-current="page">
               <div className="flex items-center">
                 <svg
@@ -97,7 +73,7 @@ const Page: NextPageWithLayout = ({ data }: any) => {
                   />
                 </svg>
                 <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                  Metadata
+                  Data Aset
                 </span>
               </div>
             </li>
@@ -106,63 +82,39 @@ const Page: NextPageWithLayout = ({ data }: any) => {
 
         <div className="mx-auto max-w-screen-xl mt-4">
           {/* Start coding here */}
-          <div className="bg-white dark:bg-gray-900 relative shadow-md sm:rounded-lg overflow-hidden">
-            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-              <div className="w-full md:w-1/2">
-                {/* <form className="flex items-center">
-                  <label htmlFor="simple-search" className="sr-only">
-                    Cari
-                  </label>
-                  <div className="relative w-full">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg
-                        aria-hidden="true"
-                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <input
-                      type="text"
-                      id="simple-search"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="Pencarian"
-                      required
-                    />
-                  </div>
-                </form> */}
+          <div className="relative overflow-hidden bg-white dark:bg-gray-800 sm:rounded-lg">
+            <div className="mt-2 flex-row items-center justify-between space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+              <div>
+                <h2 className="mr-3 font-semibold text-2xl dark:text-white">
+                  Data Aset
+                </h2>
+                {/* <p className="text-gray-500 dark:text-gray-400">
+                  Menampilkan semua data aset di perusahaan anda
+                </p> */}
               </div>
-              <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                <div className="flex items-center space-x-3 w-full md:w-auto">
-                  <button
-                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-white   bg-red-600 rounded-lg border hover:bg-red-700"
-                    type="button"
-                  >
-                    <svg
-                      className="h-3.5 w-3.5 mr-2"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                    >
-                      <path
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                      />
-                    </svg>
-                    Input Data
-                  </button>
-                </div>
-              </div>
+              <button
+                type="button"
+                className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+              >
+                <svg
+                  className="h-3.5 w-3.5 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  />
+                </svg>
+                Input Data
+              </button>
             </div>
+          </div>
+
+          <div className="mt-5 bg-white dark:bg-gray-900 relative shadow-md sm:rounded-lg overflow-hidden">
             <div className="overflow-x-auto">
               <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
---
+++ pages/aset/input/index.tsx
@@ -0,0 +1,196 @@
+import { type ReactElement } from "react";
+import { DashboardLayout } from "@/components/@layout";
+
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import nookies from "nookies";
+import { NextPageWithLayout } from "../../_app";
+
+type Aset = {
+  nama: string;
+  jenis: string;
+  pemilik: string;
+  lokasi: string;
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  data: Aset;
+}> = async (ctx) => {
+  const cookies = nookies.get(ctx);
+
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/aset", {
+    headers: {
+      Authorization: "Bearer " + cookies.access_token,
+    },
+  });
+  const data = await res.json();
+
+  return { props: { data } };
+};
+
+const Page: NextPageWithLayout = ({ data }: any) => {
+  return (
+    <>
+      <Head>
+        <title>{`Input - Data Aset - ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
+      </Head>
+      <div className="p-6 mt-16 sm:ml-64">
+        <nav className="flex" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
+              <a
+                href="#"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-red-600 dark:text-gray-400 dark:hover:text-white"
+              >
+                <svg
+                  className="w-3 h-3 mr-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <a
+                  href="#"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-red-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Data Aset
+                </a>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                  Input
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="mx-auto max-w-screen-xl mt-4">
+          <h1 className="mb-4 mt-6 text-2xl font-bold text-gray-900 dark:text-white">
+            Input Data Aset
+          </h1>
+          <form action="#">
+            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Nama
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Masukkan nama aset anda"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="category"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Kategori
+                </label>
+                <select
+                  id="category"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
+                  <option value="TV">TV/Monitors</option>
+                  <option value="PC">PC</option>
+                  <option value="GA">Gaming/Console</option>
+                  <option value="PH">Phones</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  htmlFor="category"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Okupansi
+                </label>
+                <select
+                  id="category"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
+                  <option value="TV">TV/Monitors</option>
+                  <option value="PC">PC</option>
+                  <option value="GA">Gaming/Console</option>
+                  <option value="PH">Phones</option>
+                </select>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="description"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Deskripsi
+                </label>
+                <textarea
+                  id="description"
+                  rows={4}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Masukkan deskripsi aset anda"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:ring-red-200 dark:focus:ring-red-900 hover:bg-red-700"
+            >
+              Simpan
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
+
+export default Page;
