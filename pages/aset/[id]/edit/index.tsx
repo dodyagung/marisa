c3fbe9b763465262aab52f04da636cc4caa902d8
Dody <dodyags@gmail.com>
2023-08-12 13:56:02 +0700

    aset detail page

--- pages/aset/[id]/edit/index.tsx
+++ pages/aset/[id]/edit/index.tsx
@@ -1,24 +1,11 @@
-import { type ReactElement } from "react";
-import { DashboardLayout } from "@/components/@layout";
-
+import { useState, type ReactElement } from "react";
+import { AuthLayout, DashboardLayout } from "@/components/@layout";
+import Image from "next/image";
+import Logo from "/public/logo.png";
+import { destroyCookie, setCookie } from "nookies";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import { GetServerSideProps } from "next";
-import nookies from "nookies";
-import { NextPageWithLayout } from "../_app";
-import Link from "next/link";
-
-export const getServerSideProps = async (ctx: any) => {
-  const cookies = nookies.get(ctx);
-
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/aset", {
-    headers: {
-      Authorization: "Bearer " + cookies.access_token,
-    },
-  });
-  const data = await res.json();
-
-  return { props: { data } };
-};
+import { NextPageWithLayout } from "../../../_app";
 
 const Page: NextPageWithLayout = ({ data }: any) => {
   return (
@@ -80,8 +67,8 @@ const Page: NextPageWithLayout = ({ data }: any) => {
                   Data Aset
                 </h2>
                 {/* <p className="text-gray-500 dark:text-gray-400">
-                  Menampilkan semua data aset di perusahaan anda
-                </p> */}
+                    Menampilkan semua data aset di perusahaan anda
+                  </p> */}
               </div>
               <button
                 type="button"
@@ -127,78 +114,7 @@ const Page: NextPageWithLayout = ({ data }: any) => {
                     </th>
                   </tr>
                 </thead>
-                <tbody>
-                  {data.map((aset: any, index: number) => (
-                    <tr
-                      key={index}
-                      className="odd:bg-white even:bg-gray-50 border-b dark:border-gray-700"
-                    >
-                      <th
-                        scope="row"
-                        className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        {aset.name}
-                      </th>
-                      <td className="px-4 py-3">{aset.kategori.name}</td>
-                      <td className="px-4 py-3">{aset.perusahaan.name}</td>
-                      <td className="px-4 py-3">
-                        {aset.aset_detail?.detail_alamat}
-                      </td>
-                      <td className="px-4 py-3 flex items-center justify-end">
-                        <button
-                          id="apple-imac-27-dropdown-button"
-                          data-dropdown-toggle="apple-imac-27-dropdown"
-                          className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                          type="button"
-                        >
-                          <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                          </svg>
-                        </button>
-                        <div
-                          id="apple-imac-27-dropdown"
-                          className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                        >
-                          <ul
-                            className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                            aria-labelledby="apple-imac-27-dropdown-button"
-                          >
-                            <li>
-                              <a
-                                href="#"
-                                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                              >
-                                Show
-                              </a>
-                            </li>
-                            <li>
-                              <a
-                                href="#"
-                                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                              >
-                                Edit
-                              </a>
-                            </li>
-                          </ul>
-                          <div className="py-1">
-                            <a
-                              href="#"
-                              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                            >
-                              Delete
-                            </a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
+                <tbody></tbody>
               </table>
             </div>
           </div>
