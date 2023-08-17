1e49b86b0448c1ef31ec22e5ef30645253b6ca2c
Dody <dodyags@gmail.com>
2023-08-17 10:06:48 +0700

    change aset foto detail to grid

--- pages/aset/[id]/index.tsx
+++ pages/aset/[id]/index.tsx
@@ -355,103 +355,17 @@ const Page: NextPageWithLayout = ({ data1, data2, data3 }: any) => {
                 role="tabpanel"
                 aria-labelledby="foto-tab"
               >
-                <div
-                  id="custom-controls-gallery"
-                  className="relative w-full"
-                  data-carousel="slide"
-                >
-                  {/* Carousel wrapper */}
-                  <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                    {data3.map((foto: any, index: number) => (
-                      <div
-                        key={index}
-                        className="hidden duration-700 ease-in-out"
-                        data-carousel-item=""
-                      >
-                        <img
-                          src={foto.url}
-                          className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                          alt=""
-                        />
-                      </div>
-                    ))}
-                  </div>
-                  <div className="flex justify-center items-center pt-4">
-                    <button
-                      type="button"
-                      className="flex justify-center items-center mr-4 h-full cursor-pointer group focus:outline-none"
-                      data-carousel-prev=""
-                    >
-                      <span className="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M13 5H1m0 0 4 4M1 5l4-4"
-                          />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                      </span>
-                    </button>
-                    <button
-                      type="button"
-                      className="flex justify-center items-center h-full cursor-pointer group focus:outline-none"
-                      data-carousel-next=""
-                    >
-                      <span className="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                      </span>
-                    </button>
-                  </div>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                  {data3.map((foto: any, index: number) => (
+                    <div key={index}>
+                      <img
+                        className="block max-h-96 h-full w-full rounded-lg object-cover object-center"
+                        src={foto.url}
+                        alt=""
+                      />
+                    </div>
+                  ))}
                 </div>
-
-                {/* <dl className="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
-                  <div className="flex flex-col pb-3">
-                    <dt className="mb-1 text-gray-500 dark:text-gray-400">
-                      Email address
-                    </dt>
-                    <dd className="font-semibold">yourname@flowbite.com</dd>
-                  </div>
-                  <div className="flex flex-col py-3">
-                    <dt className="mb-1 text-gray-500 dark:text-gray-400">
-                      Home address
-                    </dt>
-                    <dd className="font-semibold">
-                      92 Miles Drive, Newark, NJ 07103, California, USA
-                    </dd>
-                  </div>
-                  <div className="flex flex-col pt-3">
-                    <dt className="mb-1 text-gray-500 dark:text-gray-400">
-                      Phone number
-                    </dt>
-                    <dd className="font-semibold">
-                      +00 123 456 789 / +12 345 678
-                    </dd>
-                  </div>
-                </dl> */}
               </div>
             </div>
           </>
